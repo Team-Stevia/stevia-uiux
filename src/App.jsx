2f100902c185b579142372b3a8686165279eabdd
Boyeon-Shin <anna007096@gmail.com>
2024-07-13 11:31:17 +0900

    fix: reservation slot 클릭 이벤트 수정
    slot 클릭 시 화면이 올바르게 렌더링이 안 됐다.
    각 시간 슬롯에 적절한 onclick 이벤트 핸들러가 추가 되지 않음을 알음
    
    resolves: #8

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import Login from "./pages/Login";
 import Reservation from "./pages/Reservation";
 import {useEffect} from "react";
 
+const roomId = "2b817f4a-3cb4-4a77-b0a5-78f3db9d6b9a"
+
+
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/reservation" element={<Reservation/>}/>
+                <Route path="/reservation" element={<Reservation roomId={roomId}/>}/>
             </Routes>
         </div>
     );
--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -3,64 +3,67 @@ import location from "../assets/location.png";
 import done from "../assets/done.png";
 import "../styles/Reservation.css"
 import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
-const reservations = [
-    {id: 1, time: "09:00 ~ 10:00", reserved: true},
-    {id: 2, time: "10:00 ~ 11:00", reserved: true},
-    {id: 3, time: "11:00 ~ 12:00", reserved: true},
-    {id: 4, time: "12:00 ~ 13:00", reserved: false},
-    {id: 5, time: "13:00 ~ 14:00", reserved: false},
-    {id: 6, time: "14:00 ~ 15:00", reserved: true},
-    {id: 7, time: "15:00 ~ 16:00", reserved: false},
-    {id: 8, time: "17:00 ~ 18:00", reserved: true},
-    {id: 9, time: "18:00 ~ 19:00", reserved: false},
+
+const initialReservation = [
+    {id: 9, time: "09:00 ~ 10:00", reserved: true},
+    {id: 10, time: "10:00 ~ 11:00", reserved: true},
+    {id: 11, time: "11:00 ~ 12:00", reserved: true},
+    {id: 12, time: "12:00 ~ 13:00", reserved: true},
+    {id: 13, time: "13:00 ~ 14:00", reserved: true},
+    {id: 14, time: "14:00 ~ 15:00", reserved: true},
+    {id: 15, time: "15:00 ~ 16:00", reserved: true},
+    {id: 16, time: "17:00 ~ 18:00", reserved: true},
 ];
 
+
 const Reservation = () => {
-    // const [reservation, setReservation] = useState([]);
-    //
-    // useEffect(async () => {
-    //     try {
-    //         const response = await axios.get('http://localhost:3000/api/reservations');
-    //         setReservations(response.data);
-    //     } catch (error) {
-    //         console.error('error');
-    //     }
-    // }, []);
+    const [reservation, setReservation] = useState(initialReservation);
+    const navigate = useNavigate();
 
 
-    function onCheck() {
-
+    const useHandleMove = () => {
+        navigate("/");
     }
+
+
+    const onUpdate = (targetId) => {
+        const updatedReservations = reservation.map((slot) =>
+            slot.id === targetId ? { ...slot, reserved: !slot.reserved } : slot
+        );
+        setReservation(updatedReservations);
+    };
+
 
     return (
         <div className="reservation">
             <div className="roomImg" alt="room" style={{backgroundImage: `url(${roomImg})`}}>
-
+                <div className="arrow" onClick={useHandleMove}></div>
                 <div className="reservation-board">
                     <div className="reservation-location">
-                        <div className="reservation-location-1">
                             <img className="locationImg" src={location} alt="location"/>
-                            <p>한밭대학교 유성 덕명캠퍼스</p>
-                        </div>
-                        <p>N5 506</p>
+                            <p>한밭대학교 유성덕명캠퍼스자동화관<br/>N5 506</p>
                     </div>
 
                     <div className="reservation-time">
-                        <div className="click">
-                            <p>today 6/24</p>
-                            <img className="done-button" src={done} alt="done"/>
+                        <div className="reservation-info">
+                            <div
+                                className="date">Today<br/>{new Date().getFullYear()}.{new Date().getMonth()}.{new Date().getDate()}
+                            </div>
+                            <img className="done-button" src={done} alt="done" onClick={useHandleMove}/>
                         </div>
-                        <div className="reservationList" onClick={onCheck}>
-                            {reservations.map((slot, id) => (
-                                <div key={id} className={`timeSlot ${slot.reserved ? 'reserved' : 'no-reserved'}`}>
+                        <div className="reservation-time-slot">
+                            {reservation.map((slot, id) => (
+                                <div key={id} className={`timeSlot ${slot.reserved ? 'reserved' : 'no-reserved'}`} onClick={() => onUpdate(slot.id)}>
                                     {slot.time}
                                 </div>
                             ))}
                         </div>
                     </div>
 
-            </div>
+                </div>
             </div>
         </div>
     );

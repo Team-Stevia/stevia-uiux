40ac9e97c85bfd8dfab46439b6ac5782982e18dd
Minseo Kim <kms12782@gmail.com>
2024-07-15 19:02:22 +0900

    test: 테스트용 라이브러리 추가
    db.json, server.js 생성, package.json 수정
    
    fixes: #9

---
+++ server.js
@@ -0,0 +1,41 @@
+import jsonServer from 'json-server';
+import jwt from 'jsonwebtoken';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const secretKey = 'i am best';
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.post('/users/signin', (req, res) => {
+    const { studentId, password } = req.body;
+    const user = router.db.get('users').find({ studentId, password }).value();
+
+    if (user) {
+        // 로그인 성공 시 JWT 토큰 생성
+        const accessToken = jwt.sign({ studentId: user.studentId }, secretKey, { expiresIn: '1m' });
+        const refreshToken = jwt.sign({ studentId: user.studentId, type: "refresh" }, secretKey + 'refresh', { expiresIn: '7d' });
+
+        res.json({ accessToken, refreshToken });
+    } else {
+        res.status(401).jsonp({ error: 'Invalid credentials' });
+    }
+});
+
+server.get('/boards', async (req, res) => {
+    try {
+        const boardsData = router.db.get('boards').value();
+        res.json(boardsData);
+    } catch (error) {
+        console.error('Failed to read db.json:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+server.use(router);
+
+server.listen(4000, () => {
+    console.log('JSON Server is running on port 4000');
+});

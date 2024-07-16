f7ad91f9581a19690a15fede8d6753c8b268e211
Boyeon-Shin <anna007096@gmail.com>
2024-07-16 16:32:39 +0900

    feat: Home Get 요청(현황판 조회) API 구현
    - otherBuildingList paganation
    - 각각 roomList의 reservationRate에 따라 색변화
    - reservationStatus(true/false)에 따라 예약 페이지 접근 제한
    
    resolves: #14

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,89 @@ import useBottomSheet from '../hooks/useBottomSheet';
 import keyImage from "../assets/key.png"
 import {useNavigate} from "react-router-dom";
 import BottomSheetContent from "../components/BottomSheetContent.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:3003',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
 
 const Home = () => {
     const {isSheetOpen, openSheet, closeSheet} = useBottomSheet();
+    const [otherBuildingList, setOtherBuildingList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const buildingsPerPage = 4;
+    const [roomsList, setRoomList] = useState([]);
+    const [reservationStatus, setReservationStatus] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await apiClient.get('/boards');
+
+                const otherBuildingList = res.data[0].otherBuildingList;
+                const buildingAndRoomList = res.data[0].buildingAndRoomList;
+                const buildingLocation = buildingAndRoomList[0].buildingLocation;
+                const roomList = buildingAndRoomList[0].roomList;
+                const reservationStatus = res.data[0].reservationInfo.reservationStatus;
+
+
+                console.log(roomList);
+                console.log(buildingLocation);
+                console.log(otherBuildingList);
+                console.log(reservationStatus);
+
+
+                setOtherBuildingList(otherBuildingList);
+                setRoomList(roomList);
+                setReservationStatus(reservationStatus);
+
+            } catch (error) {
+                console.error('Error fetching the data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
 
     const useHandleMove = () => {
         navigate("/reservation");
     }
+
+    /* buildingLocation 4개씩 렌더링하기 */
+    const renderBuildingList = () => {
+        const start = currentPage * buildingsPerPage
+        const end = start + buildingsPerPage;
+        const currentBuildingList = otherBuildingList.slice(start, end);
+
+        return currentBuildingList.map((building, index) => (
+            <button key={index} className="menu-items">
+                {building}
+            </button>
+        ));
+    }
+
+    const renderRoomList = () => {
+        return roomsList.map((room) => {
+            let bgColor;
+            if (0 < room.reservationRate && room.reservationRate <= 20) bgColor = "#C1C7F3FF";
+            else if (room.reservationRate < 20) bgColor = '#FAD5D5';
+            else if (room.reservationRate < 50) bgColor = '#FF6969';
+            else if (room.reservationRate < 80) bgColor = '#FF3030';
+            else bgColor = 'red';
+
+            return (
+                <p key={room.roomId} className="select-box-items" style={{backgroundColor: bgColor}}>
+                    {room.roomNo}
+                </p>
+            );
+        });
+    };
 
     return (
         <div className="Home">
@@ -20,30 +95,32 @@ const Home = () => {
                 <b>Please choose a place!!</b>
             </div>
             <div className="menu">
-                <div className="arrow1"></div>
-                <button className='menu-items'>N3</button>
-                <button className='menu-items'>N4</button>
-                <button className='menu-items'>N5</button>
-                <button className='menu-items'>N6</button>
-                <div className="arrow2"></div>
+                <div className="arrow1"
+                     onClick={() => setCurrentPage(prevPage => Math.max(prevPage - 1, 0))}
+                     disabled={currentPage === 0}>
+                </div>
+                <div id="buildingList" className="menu-items">
+                    {renderBuildingList()}
+                </div>
+                <div className="arrow2"
+                     onClick={() => setCurrentPage(prevPage => Math.min(prevPage + 1, Math.floor(otherBuildingList.length / buildingsPerPage)))}
+                     disabled={(currentPage + 1) * buildingsPerPage >= otherBuildingList.length}
+                >
+                </div>
             </div>
             <div className="select-box" onClick={useHandleMove}>
-                <p className="select-box-items">506</p>
-                <p className="select-box-items">511</p>
-                <p className="select-box-items">512</p>
-                <p className="select-box-items">505</p>
-                <p className="select-box-items">504</p>
-                <p className="select-box-items">513</p>
+                {renderRoomList()}
             </div>
             <div className="key" onClick={openSheet}>
                 <p className='key1'></p>
                 <img src={keyImage} alt="key" className='key_image'/>
             </div>
-            <BottomSheet open={isSheetOpen} onDismiss={closeSheet}>
-                <div style={{padding: '16px'}}>
-                    <BottomSheetContent closeSheet={closeSheet}/>
-                </div>
-            </BottomSheet>
+            {reservationStatus &&
+                <BottomSheet open={isSheetOpen} onDismiss={closeSheet}>
+                    <div style={{padding: '16px'}}>
+                        <BottomSheetContent closeSheet={closeSheet}/>
+                    </div>
+                </BottomSheet>}
         </div>
     );
 }

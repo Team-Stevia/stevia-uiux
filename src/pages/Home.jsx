ff98c0f1a6c7c9d791a20614df6b541de4e1a37b
Boyeon <141127815+Boyeon-Shin@users.noreply.github.com>
2024-07-30 10:22:07 +0900

    #14/feat/home (#17)
    * feat: 예약 페이지 구현
    
    전체적인 페이지틀과 라우터 개발함
    
    resolves: #8
    
    * fix: reservation slot 클릭 이벤트 수정
    
    slot 클릭 시 화면이 올바르게 렌더링이 안 됐다.
    각 시간 슬롯에 적절한 onclick 이벤트 핸들러가 추가 되지 않음을 알음
    
    resolves: #8
    
    * feat: Reservation GET 요청 API 구현
    
    1. roomId에 대한 강의실 정보를 제공
    2. 강의실 예약 상태를 업데이트
    
    resolves: #10
    
    * feat: BottomSheet를 이용한 예약 상태 화면 구현
    
    - BottomSheet 컴포넌트를 이용하여 사용자 예약 상태를 표시하는 화면을 구현했습니다.
    - keyStatus 값(true/false)에 따라 UI가 변경됩니다.
    
    resolves: #6
    
    * test: 테스트용 라이브러리 추가
    
    db.json 파일 추가
    
    resolves: #14
    
    * feat: Home Get 요청(현황판 조회) API 구현
    
    - otherBuildingList paganation
    - 각각 roomList의 reservationRate에 따라 색변화
    - reservationStatus(true/false)에 따라 예약 페이지 접근 제한
    
    resolves: #14
    
    * feat: Home 화면 CSS 구현
    
    resolves: #14
    
    * fix: Home 상단바 수정, key 위치 수정
    
    - 상단바 BuildingLocation 정보가 없으면 첫 BuildingLocation page로 이동
    - key 위치 하단에 고정
    
    Resolves: #14

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,101 @@ import useBottomSheet from '../hooks/useBottomSheet';
 import keyImage from "../assets/key.png"
 import {useNavigate} from "react-router-dom";
 import BottomSheetContent from "../components/BottomSheetContent.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:3003',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+});
 
 const Home = () => {
     const {isSheetOpen, openSheet, closeSheet} = useBottomSheet();
+    const [otherBuildingList, setOtherBuildingList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const buildingsPerPage = 4;
+    const [roomsList, setRoomList] = useState([]);
+    const [reservationStatus, setReservationStatus] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await apiClient.get('/boards');
+
+                const otherBuildingList = res.data[0].otherBuildingList;
+                const buildingAndRoomList = res.data[0].buildingAndRoomList;
+                const buildingLocation = buildingAndRoomList[0].buildingLocation;
+                const roomList = buildingAndRoomList[0].roomList;
+                const reservationStatus = res.data[0].reservationInfo.reservationStatus;
+
+                console.log(res);
+
+
+                setOtherBuildingList(otherBuildingList);
+                setRoomList(roomList);
+                setReservationStatus(reservationStatus);
+
+            } catch (error) {
+                console.error('Error fetching the data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
 
     const useHandleMove = () => {
         navigate("/reservation");
     }
+
+    /* buildingLocation 4개씩 렌더링하기 */
+    const renderBuildingList = () => {
+        const start = currentPage * buildingsPerPage
+        const end = start + buildingsPerPage;
+        let currentBuildingList = otherBuildingList.slice(start, end);
+
+        return currentBuildingList.map((building, index) => (
+            <button key={index} className="menu-items">
+                {building}
+            </button>
+        ));
+    }
+
+    /* 상단바 왼쪽 화살표 이벤트 */
+    const handlePrevPage = () => {
+        setCurrentPage(prevPage => Math.max(prevPage - 1, 0))
+    }
+
+    /* 상단바 오른쪽 화살표 이벤트 */
+    const handleNextPage = () => {
+        setCurrentPage(prevPage => {
+            const nextPage = prevPage + 1;
+            const totalPages = Math.ceil(otherBuildingList.length / buildingsPerPage);
+            return nextPage >= totalPages ? 0 : nextPage;
+        })
+    }
+
+
+    const renderRoomList = () => {
+        return roomsList.map((room) => {
+            let bgColor;
+            if (0 < room.reservationRate && room.reservationRate <= 20) bgColor = "#C1C7F3FF";
+            else if (room.reservationRate < 20) bgColor = '#FAD5D5';
+            else if (room.reservationRate < 50) bgColor = '#FF6969';
+            else if (room.reservationRate < 80) bgColor = '#FF3030';
+            else bgColor = 'red';
+
+            return (
+                <p key={room.roomId} className="select-box-items" style={{backgroundColor: bgColor}}
+                   onClick={useHandleMove}>
+                    {room.roomNo}
+                </p>
+            );
+        });
+    };
 
     return (
         <div className="Home">
@@ -20,30 +107,28 @@ const Home = () => {
                 <b>Please choose a place!!</b>
             </div>
             <div className="menu">
-                <div className="arrow1"></div>
-                <button className='menu-items'>N3</button>
-                <button className='menu-items'>N4</button>
-                <button className='menu-items'>N5</button>
-                <button className='menu-items'>N6</button>
-                <div className="arrow2"></div>
+                <div className="arrow1"
+                     onClick={handlePrevPage}>
+                </div>
+                <div id="buildingList" className="menu-items">
+                    {renderBuildingList()}
+                </div>
+                <div className="arrow2" onClick={handleNextPage}>
+                </div>
             </div>
-            <div className="select-box" onClick={useHandleMove}>
-                <p className="select-box-items">506</p>
-                <p className="select-box-items">511</p>
-                <p className="select-box-items">512</p>
-                <p className="select-box-items">505</p>
-                <p className="select-box-items">504</p>
-                <p className="select-box-items">513</p>
+            <div className="select-box">
+                {renderRoomList()}
             </div>
             <div className="key" onClick={openSheet}>
                 <p className='key1'></p>
                 <img src={keyImage} alt="key" className='key_image'/>
             </div>
-            <BottomSheet open={isSheetOpen} onDismiss={closeSheet}>
-                <div style={{padding: '16px'}}>
-                    <BottomSheetContent closeSheet={closeSheet}/>
-                </div>
-            </BottomSheet>
+            {reservationStatus &&
+                <BottomSheet open={isSheetOpen} onDismiss={closeSheet}>
+                    <div style={{padding: '16px'}}>
+                        <BottomSheetContent closeSheet={closeSheet}/>
+                    </div>
+                </BottomSheet>}
         </div>
     );
 }

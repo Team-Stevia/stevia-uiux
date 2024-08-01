82dbcfdf3c1e55d4bcfd65f6df97e210560893df
Boyeon <141127815+Boyeon-Shin@users.noreply.github.com>
2024-08-01 16:06:57 +0900

    로그인 요청 API 구현 (#15)
    * feat: 예약 페이지 구현
    
    전체적인 페이지틀과 라우터 개발함
    
    resolves: #8
    
    * fix: reservation slot 클릭 이벤트 수정
    
    slot 클릭 시 화면이 올바르게 렌더링이 안 됐다.
    각 시간 슬롯에 적절한 onclick 이벤트 핸들러가 추가 되지 않음을 알음
    
    resolves: #8
    
    * feat: Reservation GET 요청 API 구현
    
    1. roomId에 대한 강의실 정보를 제공
    2. 강의실 예약 상태를 업데이트
    
    resolves: #10
    
    * feat: BottomSheet를 이용한 예약 상태 화면 구현
    
    - BottomSheet 컴포넌트를 이용하여 사용자 예약 상태를 표시하는 화면을 구현했습니다.
    - keyStatus 값(true/false)에 따라 UI가 변경됩니다.
    
    resolves: #6
    
    * test: 테스트용 라이브러리 추가
    
    db.json 파일 추가
    
    resolves: #14
    
    * feat: Home Get 요청(현황판 조회) API 구현
    
    - otherBuildingList paganation
    - 각각 roomList의 reservationRate에 따라 색변화
    - reservationStatus(true/false)에 따라 예약 페이지 접근 제한
    
    resolves: #14
    
    * feat: Home 화면 CSS 구현
    
    resolves: #14
    
    * fix: Home 상단바 수정, key 위치 수정
    
    - 상단바 BuildingLocation 정보가 없으면 첫 BuildingLocation page로 이동
    - key 위치 하단에 고정
    
    Resolves: #14
    
    * test: 테스트용 라이브러리, 설정파일 추가
    
    resolves: #15
    
    * feat: login 요청 api 구현
    
    resolves:#15
    
    * feat: accessToken, refreshToken 관련 api, redux slice 파일 구현
    
    Resolves: #15
    
    resolves: #15
    
    * refactor: 코드 수정
    
    resolves: #15

---
+++ src/feature/slice/authSlice.js
@@ -0,0 +1,44 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {loginUser} from "../apis/loginApi.js";
+
+
+/**
+ * 사용자 로그인을 위한 Thunk 액션 생성 함수
+ * 사용자 인증을 위한 비동기 액션 디스패치
+ */
+export const login = createAsyncThunk(
+    'auth/login',
+    async ({studentId, password}) => {
+        return loginUser({studentId, password});
+    }
+);
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        accessToken: null,
+        status: 'idle',
+        error: null,
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(login.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.accessToken = action.payload.accessToken;
+                state.error = null; // 성공 시 에러 초기화
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message; // 실패 시 에러 메시지 설정
+            });
+    },
+
+})
+
+export const {logout, setAccessToken} = authSlice.actions;
+
+export default authSlice.reducer;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,68 @@
 import logo from '../assets/logo.jpg'
 import background from '../assets/background.svg'
-
 import '../styles/Login.css'
+import axios from "axios";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useCookies} from "react-cookie";
+import {useDispatch, useSelector} from "react-redux";
+import {login} from "../feature/slice/authSlice.js";
 
-const handleOnclick = () => {
-}
 
 const Login = () => {
+    const [studentId, setStudentId] = useState("");
+    const [password, setPassword] = useState("");
+    const [cookie, setCookie] = useCookies(['refreshToken']);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const authStatus = useSelector((state) => state.auth.status);
+    const authError = useSelector((state) => state.auth.error);
+
+    const handleOnclick = async () => {
+        try {
+            const {payload} = await dispatch(login({studentId, password}));
+            console.log(payload);
+            setCookie('refreshToken', payload.refreshToken, {path: '/'});
+            navigate('/');
+        } catch (error) {
+            console.error('Login failed', error);
+        }
+    }
+
     return (
         <div className="background-img" style={{backgroundImage: `url(${background})`}}>
-            <div className="login-container" >
-                <img className="logo-img"  src={logo} alt="logo"/>
-                {/*<form>*/}
-                    <div className="email" >
-                        <input type="email" id="email" name="email"/>
-                    </div>
-
-                    <div className="password">
-                        <input type="password" id="password" name="password"/>
-                    </div>
-                    <div className="find-password">
-                        <p>Forgot Password?</p>
-                    </div>
-                    <button type="login">Login</button>
-                {/*</form>*/}
+            <div className="login-container">
+                <img className="logo-img" src={logo} alt="logo"/>
+                <div className="student_id">
+                    <input
+                        type="text"
+                        id="student_id"
+                        name="student_id"
+                        value={studentId}
+                        onChange={(e) => {
+                            setStudentId(e.target.value);
+                        }}/>
+                </div>
+                <div className="password">
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}/>
+                </div>
+                <div className="find-password">
+                    <p>Forgot Password?</p>
+                </div>
+                <button type="button" onClick={handleOnclick}>Login</button>
+                {authStatus === 'loading' && <p>Loading...</p>}
+                {authStatus === 'failed' && <p>Error: {authError}</p>}
             </div>
         </div>
-
     );
 }
 

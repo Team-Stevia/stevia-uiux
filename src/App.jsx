9a5bd194fb69526e52ed301c2ad11d4508248c1e
Boyeon <141127815+Boyeon-Shin@users.noreply.github.com>
2024-08-01 16:07:26 +0900

    Revert "로그인 요청 API 구현 (#15)"
    This reverts commit 82dbcfdf3c1e55d4bcfd65f6df97e210560893df.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import './App.css';
-import {Routes, Route, Navigate} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Reservation from "./pages/Reservation";
 import {useEffect} from "react";
-import {useSelector} from "react-redux";
-
 
 const roomId = "2b817f4a-3cb4-4a77-b0a5-78f3db9d6b9a"
 
-const PrivateRoute = ({element}) => {
-    const accessToken = useSelector((state) => state.auth.accessToken);
-    return accessToken ? element : <Navigate to="/login"/>;
-};
 
 function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path="/" element={<PrivateRoute element={<Home/>}/>}/>
+                <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
-                <Route path="/reservation" element={<PrivateRoute element={<Reservation/>}/>}/>
+                <Route path="/reservation" element={<Reservation roomId={roomId}/>}/>
             </Routes>
         </div>
     );

224ca9b93d0077734e9e192b7517d524c3fd1052
Boyeon-Shin <anna007096@gmail.com>
2024-08-04 20:57:03 +0900

    feat: 상태 저장 redux-persist 기능 추가
    - 새로고침 후 access token이 유지 되지 않아 재로그인 이슈 발생
    - access token 상태를 저장, 상태 복원 하는 기능 사용
    - 브라우저의 로컬스토리지를 사용
    
    Resolves: #15

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authReducer from '../feature/slice/authSlice';
 
+const rootPersistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth'],
+};
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+});
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    'persist/PERSIST',
+                    'persist/REHYDRATE',
+                    'persist/FLUSH',
+                    'persist/PAUSE',
+                    'persist/PURGE',
+                    'persist/REGISTER',
+                ],
+            },
+        }),
     devTools: process.env.NODE_ENV !== 'production',
 });
 
+export const persistor = persistStore(store);
 export default store;
